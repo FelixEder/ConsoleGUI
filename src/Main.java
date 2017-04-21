67e6ea3b313b9490e15c09337f93beb46fda76c3
felixed <felixed@kth.se>
2017-04-21 11:20:03 +0200

    No more nullpointers, but everything still doesn't work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -14,6 +19,11 @@ public class Main extends Application{
 	@FXML
 	protected TextArea output, inventory, commands;
 	
+	
+	protected static List<String> history;
+	protected static int historyPointer;
+	protected static String textToRead = null;
+	
 	public static void main(String[] args) {
 		Application.launch(args);
 
@@ -22,9 +32,51 @@ public class Main extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		BorderPane root = FXMLLoader.load(getClass().getResource("Console.fxml"));
+		
+		output = new TextArea();
+		input = new TextField();
+		inventory = new TextArea();
+		commands = new TextArea();
+		
+		history = new ArrayList<>();
+		historyPointer = 0;
+		
+		input.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			public void handle(KeyEvent keyEvent) {
+				switch (keyEvent.getCode()) {
+				case ENTER: String text = input.getText();
+							output.appendText(text + System.lineSeparator());
+							history.add(text);
+							historyPointer = history.size(); 
+							input.clear();
+							textToRead = text;
+							break;
+							
+				case UP :	if(historyPointer == 0) break;
+							historyPointer--;
+							input.setText(history.get(historyPointer));
+							input.selectAll();
+							input.selectEnd(); //Does not change anything seemingly
+							break;
+							
+				case DOWN:	if(historyPointer == history.size()-1) break;
+							historyPointer++;
+							input.setText(history.get(historyPointer));
+							input.selectAll();
+							input.selectEnd(); //Does not change anything seemingly
+							break;
+
+				default: break;
+				}
+
+			}
+});
+		
+
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
-		stage.setTitle("The Expedition"); //Could later be changed so that the actual game title is displayed here.
+		stage.setTitle("MyConsoleFXGUI"); //Could later be changed so that the actual game title is displayed here.
 		stage.show();
 		
 	}
